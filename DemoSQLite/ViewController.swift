b7921563f61524f07cd0195073542548d4598269
desarrollo <desarrollo@192.168.2.126>
2019-09-18 11:58:16 -0600

    new version

--- DemoSQLite/ViewController.swift
+++ DemoSQLite/ViewController.swift
@@ -11,6 +11,8 @@ import SQLite
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
