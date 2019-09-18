dcb607479398988b6ee36f741de377537768db71
desarrollo <desarrollo@192.168.2.126>
2019-09-18 11:41:40 -0600

    *

---
+++ DemoSQLite/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DemoSQLite
+//
+//  Created by desarrollo on 9/18/19.
+//  Copyright © 2019 desarrollo. All rights reserved.
+//
+
+import UIKit
+import SQLite
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
